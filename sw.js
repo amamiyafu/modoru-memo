45c73a6a199181ddf52cbc57b48790f71e0ab50e
amamiyafu <fu2026amamiya@gmail.com>
2026-05-26 04:56:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,49 +1,39 @@
-﻿const CACHE_NAME = "iwakan-memo-v6";
-const APP_SHELL = [
+﻿const CACHE_NAME = "modoru-memo-app-cache-v7";
+const CACHE_FILES = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") {
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request).then(networkResponse => {
-        const responseClone = networkResponse.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return networkResponse;
-      });
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
